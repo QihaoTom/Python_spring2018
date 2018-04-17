f4f7b21427a623553c3a7e0e33634628427b616d
Qihao Tang <37135037+QihaoTom@users.noreply.github.com>
2018-04-17 15:09:45 +0800

    Update 函数 | 7th week 第二次课.py

--- 函数 | 7th week 第二次课.py
+++ 函数 | 7th week 第二次课.py
@@ -1,25 +1,25 @@
-#两种方式生成1到100的随机数
+# 两种方式生成1到100的随机数
 
-##第一种
+## 第一种
 import random
 random.randint(1,100)
 
-##第二种
+## 第二种
 ###第二种更好
 from random import randint
 
-##random模块中常用函数的使用方法
+## random模块中常用函数的使用方法
 randint(1,100)
-#从序列中获取一个随机值
+# 从序列中获取一个随机值
 random.choice(['C++','Java','Python'])
-#从range（0，10，2）中获取一个随机整数
+# 从range（0，10，2）中获取一个随机整数
 random.randrange(0,10,2)
 #生成（0，1.0）之间的一个随机浮点数
 random.random()
-#生成【5，10】之间的一个随机浮点数
+# 生成【5，10】之间的一个随机浮点数
 random.uniform(5,10)
 
-#实现直接可调用，通用性！
+# 实现直接可调用，通用性！
 ## 关键的语句是 if __name__== "__main__":
 from math import sqrt
 def isprime(x):
