2726b9e914e6c55fc30836dad19e84d1cc6e4365
Qihao Tang <37135037+QihaoTom@users.noreply.github.com>
2018-04-17 11:13:51 +0800

    Update Python | 7th Week.py

--- Python | 7th Week.py
+++ Python | 7th Week.py
@@ -1,11 +1,15 @@
 ## 3.插入排序法
 lst=[3,5,11,7,6]
 n=len(lst)
+# 当i=3，j=2
 for i in range(1,n):
     temp=lst[i]
     j = i - 1
     while temp < lst[j] and j>=0:
+        #11往后挪，变为3，5，11，11，6
         lst[j+1]=lst[j]
         j-=1
+        #j=1
     lst[j+1]=temp
+        #把7赋回去，变为3，5，7，11，6
 print(lst)
