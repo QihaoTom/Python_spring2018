fbfcbea22f87670c61895058b580b716c431f273
Qihao Tang <37135037+QihaoTom@users.noreply.github.com>
2018-04-24 21:28:36 +0800

    Update 实现列表中单词大写 | 8th.py

--- 实现列表中单词大写 | 8th.py
+++ 实现列表中单词大写 | 8th.py
@@ -1,2 +1,9 @@
+## 比用return语句的列表节省空间，也提高效率
+
 def capital(listB):
-  for
+  for i in range(len(listB)):
+    listB[i]=listB[i].capitalize()
+listA=['hope','is','a','good','thing','.']
+listA_backup=listA[:]
+capital(listA)
+print(listA)
