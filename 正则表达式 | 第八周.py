3a69c0310a57f432ab656d93a02a422b05558676
Qihao Tang <37135037+QihaoTom@users.noreply.github.com>
2018-04-26 10:50:33 +0800

    Add files via upload

---
+++ 正则表达式 | 第八周.py
@@ -0,0 +1,12 @@
+# replace命令
+
+# 切词partition , split
+# rpartition,从右往左切第一个出现的词
+# split, 把找的东西给去掉了
+
+# Regular Exoression 正则表达式
+
+# ！ magical expression  ls类型 *python*.ipynb
+# \s+ 为空格
+# \为转义符，转义成空格
+# \w 字母和数字   
