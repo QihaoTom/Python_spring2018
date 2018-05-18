e4a893db88f030bbcf7ecfb853a11988b95d561a
Qihao Tang <37135037+QihaoTom@users.noreply.github.com>
2018-05-18 15:25:15 +0800

    Update 折线图和点图 | 自找.py

--- 折线图和点图 | 自找.py
+++ 折线图和点图 | 自找.py
@@ -21,3 +21,21 @@ plt.ylabel('Square of Value',fontsize=14)
 
 plt.tick_params(axis='both', which='major', labelsize=14)
 plt.show()
+
+# 画折线图
+import matplotlib.pyplot as plt
+
+x_value = list(range(1,1001))
+y_value = [x**2 for x in x_value]
+
+plt.scatter(x_value, y_value, c=y_value, cmap=plt.cm.Reds,
+            edgecolors='none', s=1)
+
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.axis([0, 1100, 0, 1100000])
+
+plt.savefig('squares_plot.png', bbox_inches='tight')
